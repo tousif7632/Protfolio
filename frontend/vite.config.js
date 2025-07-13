0118bc29b55cf75863ca3bebca40a0fe8633d519
tousif7632 <mdtousifalam85@gmail.com>
2025-07-13 13:09:05 +0530

    vite.config.js update

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
   plugins: [react(), tailwindcss()],
   server: {
     proxy: {
-      '/api': 'http://localhost:5000',
+      '/api': 'https://protfolio-q9v1.onrender.com',
     },
   },
 })
